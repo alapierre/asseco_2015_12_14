6c5d9a6ed925c18afc5c3ffcd19dd585f40d6efd
Administrator <matmmal@gmail.com>
2015-12-15 15:56:18 +0100

    Order, OrderItem changes

--- lab02/src/main/java/pl/sages/spring/lab/model/OrderItem.java
+++ lab02/src/main/java/pl/sages/spring/lab/model/OrderItem.java
@@ -1,22 +1,30 @@
 package pl.sages.spring.lab.model;
 
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  * Created by Administrator on 2015-12-15.
  */
 
-public class OrderElement {
-
-    @Id
-    private Long id;
+public class OrderItem extends BaseEntity{
 
     private Long idProduct;
 
     private Long quantity;
 
-    public Long getId() {
-        return id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID", nullable = false)
+    private Order order;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
     }
 
     public void setId(Long id) {
