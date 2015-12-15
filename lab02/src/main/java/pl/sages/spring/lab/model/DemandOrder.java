441b43337247cbffd0caec3ec4452579d7fd76e5
194199 <70weryfikacji>
2015-12-15 14:12:31 +0100

    .

--- lab02/src/main/java/pl/sages/spring/lab/model/DemandOrder.java
+++ lab02/src/main/java/pl/sages/spring/lab/model/DemandOrder.java
@@ -2,6 +2,7 @@ package pl.sages.spring.lab.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Created by Administrator on 2015-12-15.
@@ -11,11 +12,21 @@ public class DemandOrder extends Order{
 
     private Long supplierId;
 
+    private List<OrderElement> orderElements;
+
     public Long getSupplierId() {
         return supplierId;
     }
 
     public void setSupplierId(Long supplierId) {
         this.supplierId = supplierId;
+    }
+
+    public List<OrderElement> getOrderElements() {
+        return orderElements;
+    }
+
+    public void setOrderElements(List<OrderElement> orderElements) {
+        this.orderElements = orderElements;
     }
 }
