37cd120a2d01839f21263b8b07b5d935d28d2747
jacekmii <xefesa@mailzi.ru>
2015-12-15 14:01:14 +0100

    Person

--- lab02/src/main/java/pl/sages/spring/lab/model/Client.java
+++ lab02/src/main/java/pl/sages/spring/lab/model/Client.java
@@ -12,15 +12,17 @@ public class Client {
     @Id
     @GeneratedValue
     private long id;
-    private String name;
-    private String lastName;
-    private String documentId;
-    private String email;
-    private String phoneNumber;
-    private String nip;
-    private String regon;
-
     private long addressId;
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    private long personId;
 
     public long getId() {
         return id;
@@ -30,67 +32,11 @@ public class Client {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDocumentId() {
-        return documentId;
-    }
-
-    public void setDocumentId(String documentId) {
-        this.documentId = documentId;
-    }
-
     public long getAddressId() {
         return addressId;
     }
 
     public void setAddressId(long addressId) {
         this.addressId = addressId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getNip() {
-        return nip;
-    }
-
-    public void setNip(String nip) {
-        this.nip = nip;
-    }
-
-    public String getRegon() {
-        return regon;
-    }
-
-    public void setRegon(String regon) {
-        this.regon = regon;
     }
 }
---
+++ lab02/src/main/java/pl/sages/spring/lab/model/Person.java
@@ -0,0 +1,52 @@
+package pl.sages.spring.lab.model;
+
+/**
+ * Created by Administrator on 2015-12-15.
+ */
+public class Person {
+    private long id;
+    private String name;
+    private String lastName;
+    private String nip;
+    private String regon;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getRegon() {
+        return regon;
+    }
+
+    public void setRegon(String regon) {
+        this.regon = regon;
+    }
+}
