88f8b55134d414c3b1a93543e8e35305b8d3f976
adrian <adrian@soft-project.pl>
2015-12-16 17:03:44 +0100

    spring boot JPA

--- demo-spring-web/src/main/java/pl/com/sages/DemoSpringBootApplication.java
+++ demo-spring-web/src/main/java/pl/com/sages/DemoSpringBootApplication.java
@@ -1,9 +1,11 @@
-package com.example;
+package pl.com.sages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "pl.com.sages.model")
 public class DemoSpringBootApplication {
 
     public static void main(String[] args) {
